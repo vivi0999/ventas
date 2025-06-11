db26ff0d2b65d3ec8e2d25a4811d13d70adfee5b
vivi0999 <vivianalopez2008@cesanrafael.org>
2025-06-11 17:01:30 -0400

    creamos archivo

--- config/conexion.py
+++ config/conexion.py
@@ -1,10 +1,10 @@
 
 import mysql.connector
 conexion=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",
-    database="dbventas"
+    host="bifh5d2wnnbinrzkv8de-mysql.services.clever-cloud.com",
+    user="uk3v9k2zh9qkjlg7",
+    password="nnrdJpFcrhMR2xO8WNSj",
+    database="bifh5d2wnnbinrzkv8de"
 )
 if conexion.is_connected():
     print("conexion")
